ea39aef4984a7588ceee8c7c834c96b4dd43d569
OmoDam <anneomoyeni@gmail.com>
2023-05-11 15:53:40 -0700

    updates

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,1 +1,18 @@
+#include "main.h"
 
+/**
+g* _sqrt_recursioe -Returns the natural root of a number
+ * @n: The number that sqrt is to be calculated
+ * @i: Iterator
+ * Return: The calculated sqrt
+ * -1: If n does not have a natural sqrt
+ */
+
+int _sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt_recursion(n, i + 1));
+}
